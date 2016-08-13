d31649ae1ba5c6ce7f2b35ee31a55252b0010040
Myles Fong <i@myles.hk>
2016-08-13 19:42:59 +0800

    Back to port 3000

--- server.js
+++ server.js
@@ -7,42 +7,42 @@ const io = require('socket.io').listen(server);
 const usernames = {};
 
 app.set('view engine', 'ejs');
-app.set('view options', { layout: false });
+app.set('view options', {layout: false});
 app.use('/public', express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.render('index'));
 
 io.sockets.on('connection', (socket) => {
-  socket.on('sendchat', (data) => {
-    io.sockets.emit('updatechat', socket.username, data);
-  });
+    socket.on('sendchat', (data) => {
+        io.sockets.emit('updatechat', socket.username, data);
+    });
 
-  socket.on('adduser', (username) => {
-    socket.username = username;
+    socket.on('adduser', (username) => {
+        socket.username = username;
 
-    usernames[username] = username;
+        usernames[username] = username;
 
-    socket.emit(
-      'servernotification', {
-        connected: true,
-        toSelf: true,
-        username: username
-      });
+        socket.emit(
+            'servernotification', {
+                connected: true,
+                toSelf: true,
+                username: username
+            });
 
-    socket.broadcast.emit('servernotification', { connected: true, username: username });
+        socket.broadcast.emit('servernotification', {connected: true, username: username});
 
-    io.sockets.emit('updateusers', usernames);
-  });
+        io.sockets.emit('updateusers', usernames);
+    });
 
-  socket.on('disconnect', () => {
-    delete usernames[socket.username];
+    socket.on('disconnect', () => {
+        delete usernames[socket.username];
 
-    io.sockets.emit('updateusers', usernames);
+        io.sockets.emit('updateusers', usernames);
 
-    socket.broadcast.emit('servernotification', { username: socket.username });
-  });
+        socket.broadcast.emit('servernotification', {username: socket.username});
+    });
 });
 
-server.listen(process.env.PORT || 80);
+server.listen(process.env.PORT || 3000);
